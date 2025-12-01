fce2225806cb40ba6e86390d69c10b68fbfd204e
Shreyash1928 <shreyashwadkar1991@gmail.com>
2025-12-01 10:47:48 +0000

    jenkins file changed

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const http = require('http');
 const port = process.env.PORT || 8080;
-const version = process.env.VERSION || "v1 (Blue)";
+const color = process.env.DEPLOY_COLOR || "blue";
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(`Hello from the app - version: ${version}`);
+    res.end(`Hello from the app - color: ${color}`);
 });
+
 server.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}, color=${color}`);
 });
